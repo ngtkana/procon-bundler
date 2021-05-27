3d91bea2190ca4a7cc9f0cb8ac6ee6f0365b03c9
Kana <ngtkana@gmail.com>
2021-05-28 02:52:33 +0900

    Improve bundle_crate to parse complicated module stuructures.

--- src/bundle_crate.rs
+++ src/bundle_crate.rs
@@ -3,13 +3,14 @@ mod parse_line;
 use {
     super::Resolve,
     crate::ConfigToml,
-    parse_line::parse_module_decl,
+    parse_line::{parse_block_end, parse_module_block_begin, parse_module_decl},
     std::{
         io::BufRead,
-        mem::take,
         path::{Path, PathBuf},
     },
 };
+
+const TAB_LENGTH: usize = 4;
 
 pub fn bundle_crate<R: Resolve>(resolver: R, config_toml: ConfigToml) -> Module {
     CrateBundler::new(resolver, config_toml).bundle_crate()
@@ -33,8 +34,10 @@ impl<R: Resolve> CrateBundler<R> {
         self.bundle_module(reader, PathBuf::from("."))
     }
     fn bundle_module(&mut self, reader: impl BufRead, mut current_module_path: PathBuf) -> Module {
-        let mut spans = Vec::new();
-        let mut lines = Vec::new();
+        // 結果がモジュール別に格納されるスタック
+        // （関数終了時には、要素数 1 になっているべきです。）
+        let mut stack = vec![Module::new(current_module_path.clone())];
+
         for line in reader.lines().map(|line| {
             line.unwrap_or_else(|e| {
                 panic!(
@@ -43,26 +46,104 @@ impl<R: Resolve> CrateBundler<R> {
                 )
             })
         }) {
+            // この行を使う必要があるときに立てるフラグ
+            let mut needs_current_line = false;
+
+            // 正規表現によるパースをトライ
+            //
+            // どの `Case \d` にも合致しないときには、
+            // `needs_current_line` フラグが立つので、
+            // 直後に回収します。
             if let Some(name) = parse_module_decl(&line) {
-                spans.push(Span::RawLines(take(&mut lines)));
+                // Case 1: モジュール宣言
+                //
+                // * モジュールパスを変更して再帰呼出し
+                // * モジュールパスを戻す
+                //
                 current_module_path.push(name);
-                spans.push(Span::Module(Box::new(self.bundle_module(
-                    self.resolver.resolve(&current_module_path),
-                    current_module_path.clone(),
-                ))));
+                stack
+                    .last_mut()
+                    .unwrap()
+                    .spans
+                    .push(Span::Module(Box::new(self.bundle_module(
+                        self.resolver.resolve(&current_module_path),
+                        current_module_path.clone(),
+                    ))));
                 current_module_path.pop();
+            } else if let Some(name) = parse_module_block_begin(&line) {
+                // Case 2: インラインモジュールの開始
+                //
+                // * モジュールパスを変更
+                // * スタックに新しいモジュールを積む
+                //
+                current_module_path.push(name);
+                stack.push(Module::new(current_module_path.clone()));
+            } else if let Some(space_count) = parse_block_end(&line) {
+                if 2 <= stack.len() && space_count == (stack.len() - 2) * TAB_LENGTH {
+                    // Case 3: インラインモジュールの終了
+                    //
+                    // * 終了したモジュールをスタックから取り出してスカッシュ
+                    // * モジュールパスを戻す
+                    //
+                    current_module_path.pop();
+                    let module = stack.pop().unwrap();
+                    stack
+                        .last_mut()
+                        .unwrap()
+                        .spans
+                        .push(Span::Module(Box::new(module)));
+                } else {
+                    needs_current_line = true;
+                }
             } else {
-                lines.push(line);
+                needs_current_line = true;
+            }
+
+            // 「この行を使う必要があるときに立てるフラグ」回収です。
+            if needs_current_line {
+                let stack_len = stack.len();
+                let spans = &mut stack.last_mut().unwrap().spans;
+                if !matches!(spans.last(), Some(Span::Lines(_))) {
+                    spans.push(Span::Lines(Vec::new()));
+                }
+                match spans.last_mut().unwrap() {
+                    Span::Lines(ref mut lines) => {
+                        lines.push(remove_indentation(&line, stack_len - 1));
+                    }
+                    Span::Module(_) => unreachable!(),
+                }
             }
         }
-        if !lines.is_empty() {
-            spans.push(Span::RawLines(lines));
-        }
-        Module {
-            spans,
-            path: current_module_path,
-        }
+        let res = stack.pop().unwrap();
+        assert!(stack.is_empty());
+        res
     }
+}
+
+fn remove_indentation(line: &str, indent_level: usize) -> String {
+    let mut chars = line.chars().peekable();
+    let mut rest = indent_level * TAB_LENGTH;
+    while let Some(c) = chars.peek() {
+        match c {
+            ' ' => {
+                if rest == 0 {
+                    break;
+                } else {
+                    rest -= 1;
+                }
+            }
+            '\t' => {
+                if rest < TAB_LENGTH {
+                    break;
+                } else {
+                    rest -= TAB_LENGTH;
+                }
+            }
+            _ => break,
+        }
+        chars.next();
+    }
+    chars.collect::<String>()
 }
 
 #[derive(Clone, Debug, Default, Hash, PartialEq)]
@@ -70,9 +151,17 @@ pub struct Module {
     path: PathBuf,
     spans: Vec<Span>,
 }
+impl Module {
+    fn new(path: PathBuf) -> Self {
+        Self {
+            path,
+            spans: Vec::new(),
+        }
+    }
+}
 #[derive(Clone, Debug, Hash, PartialEq)]
 pub enum Span {
-    RawLines(Vec<String>),
+    Lines(Vec<String>),
     Module(Box<Module>),
 }
 
@@ -97,7 +186,7 @@ mod tests {
         let result = bundle_crate(ManualResolver {}, ConfigToml::new(""));
         let expected = Module {
             path: PathBuf::from("."),
-            spans: vec![Span::RawLines(vec!["hi,".to_owned(), "hello!".to_owned()])],
+            spans: vec![Span::Lines(vec!["hi,".to_owned(), "hello!".to_owned()])],
         };
         assert_eq!(result, expected);
     }
@@ -121,16 +210,322 @@ mod tests {
         let expected = Module {
             path: PathBuf::from("."),
             spans: vec![
-                Span::RawLines(vec!["hi,".to_owned()]),
+                Span::Lines(vec!["hi,".to_owned()]),
                 Span::Module(Box::new(Module {
                     path: PathBuf::from("./a"),
-                    spans: vec![Span::RawLines(vec![
+                    spans: vec![Span::Lines(vec![
                         "a also says: hi,".to_owned(),
                         "a also says: hello!".to_owned(),
                     ])],
                 })),
-                Span::RawLines(vec!["hello!".to_owned()]),
+                Span::Lines(vec!["hello!".to_owned()]),
             ],
+        };
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn test_bundle_one_inline_module() {
+        manual_resolver! {
+            struct ManualResolver {
+                "." => concat!(
+                    "hi,\n",
+                    "mod a {\n",
+                    "    hey\n",
+                    "   shallow\n",
+                    "     deep\n",
+                    "}\n",
+                    "hello!\n",
+                ),
+            }
+        }
+        let result = bundle_crate(ManualResolver {}, ConfigToml::new(""));
+        let expected = Module {
+            path: PathBuf::from("."),
+            spans: vec![
+                Span::Lines(vec!["hi,".to_owned()]),
+                Span::Module(Box::new(Module {
+                    path: PathBuf::from("./a"),
+                    spans: vec![Span::Lines(vec![
+                        "hey".to_owned(),
+                        "shallow".to_owned(),
+                        " deep".to_owned(),
+                    ])],
+                })),
+                Span::Lines(vec!["hello!".to_owned()]),
+            ],
+        };
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn test_deep_inline_module() {
+        manual_resolver! {
+            struct ManualResolver {
+                "." => concat!(
+                    "begin .\n",
+                    "mod a {\n",
+                    "    begin a\n",
+                    "    mod b {\n",
+                    "        begin b\n",
+                    "        mod c {\n",
+                    "            begin c\n",
+                    "            mod d {\n",
+                    "                begin d\n",
+                    "                end d\n",
+                    "            }\n",
+                    "            end c\n",
+                    "        }\n",
+                    "        end b\n",
+                    "    }\n",
+                    "    end a\n",
+                    "}\n",
+                    "end .\n",
+                ),
+            }
+        }
+        let result = bundle_crate(ManualResolver {}, ConfigToml::new(""));
+        let expected = Module {
+            path: PathBuf::from("."),
+            spans: vec![
+                Span::Lines(vec!["begin .".to_owned()]),
+                Span::Module(Box::new(Module {
+                    path: PathBuf::from("./a"),
+                    spans: vec![
+                        Span::Lines(vec!["begin a".to_owned()]),
+                        Span::Module(Box::new(Module {
+                            path: PathBuf::from("./a/b"),
+                            spans: vec![
+                                Span::Lines(vec!["begin b".to_owned()]),
+                                Span::Module(Box::new(Module {
+                                    path: PathBuf::from("./a/b/c"),
+                                    spans: vec![
+                                        Span::Lines(vec!["begin c".to_owned()]),
+                                        Span::Module(Box::new(Module {
+                                            path: PathBuf::from("./a/b/c/d"),
+                                            spans: vec![Span::Lines(vec![
+                                                "begin d".to_owned(),
+                                                "end d".to_owned(),
+                                            ])],
+                                        })),
+                                        Span::Lines(vec!["end c".to_owned()]),
+                                    ],
+                                })),
+                                Span::Lines(vec!["end b".to_owned()]),
+                            ],
+                        })),
+                        Span::Lines(vec!["end a".to_owned()]),
+                    ],
+                })),
+                Span::Lines(vec!["end .".to_owned()]),
+            ],
+        };
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn test_deep_external_module() {
+        manual_resolver! {
+            struct ManualResolver {
+                "." => concat!(
+                    "begin .\n",
+                    "mod a;\n",
+                    "end .\n",
+                ),
+                "./a" => concat!(
+                    "begin a\n",
+                    "mod b;\n",
+                    "end a\n",
+                ),
+                "./a/b" => concat!(
+                    "begin b\n",
+                    "mod c;\n",
+                    "end b\n",
+                ),
+                "./a/b/c" => concat!(
+                    "begin c\n",
+                    "mod d;\n",
+                    "end c\n",
+                ),
+                "./a/b/c/d" => concat!(
+                    "begin d\n",
+                    "end d\n",
+                ),
+            }
+        }
+        let result = bundle_crate(ManualResolver {}, ConfigToml::new(""));
+        let expected = Module {
+            path: PathBuf::from("."),
+            spans: vec![
+                Span::Lines(vec!["begin .".to_owned()]),
+                Span::Module(Box::new(Module {
+                    path: PathBuf::from("./a"),
+                    spans: vec![
+                        Span::Lines(vec!["begin a".to_owned()]),
+                        Span::Module(Box::new(Module {
+                            path: PathBuf::from("./a/b"),
+                            spans: vec![
+                                Span::Lines(vec!["begin b".to_owned()]),
+                                Span::Module(Box::new(Module {
+                                    path: PathBuf::from("./a/b/c"),
+                                    spans: vec![
+                                        Span::Lines(vec!["begin c".to_owned()]),
+                                        Span::Module(Box::new(Module {
+                                            path: PathBuf::from("./a/b/c/d"),
+                                            spans: vec![Span::Lines(vec![
+                                                "begin d".to_owned(),
+                                                "end d".to_owned(),
+                                            ])],
+                                        })),
+                                        Span::Lines(vec!["end c".to_owned()]),
+                                    ],
+                                })),
+                                Span::Lines(vec!["end b".to_owned()]),
+                            ],
+                        })),
+                        Span::Lines(vec!["end a".to_owned()]),
+                    ],
+                })),
+                Span::Lines(vec!["end .".to_owned()]),
+            ],
+        };
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn test_many_inline_module() {
+        manual_resolver! {
+            struct ManualResolver {
+                "." => concat!(
+                    "begin .\n",
+                    "mod a {\n",
+                    "    begin a\n",
+                    "    end a\n",
+                    "}\n",
+                    "between a and b\n",
+                    "between a and b\n",
+                    "between a and b\n",
+                    "mod b {\n",
+                    "    begin b\n",
+                    "    end b\n",
+                    "}\n",
+                    "between b and c\n",
+                    "between b and c\n",
+                    "between b and c\n",
+                    "mod c {\n",
+                    "    begin c\n",
+                    "    end c\n",
+                    "}\n",
+                    "end .\n",
+                ),
+            }
+        }
+        let result = bundle_crate(ManualResolver {}, ConfigToml::new(""));
+        let expected = Module {
+            path: PathBuf::from("."),
+            spans: vec![
+                Span::Lines(vec!["begin .".to_owned()]),
+                Span::Module(Box::new(Module {
+                    path: PathBuf::from("./a"),
+                    spans: vec![Span::Lines(vec!["begin a".to_owned(), "end a".to_owned()])],
+                })),
+                Span::Lines(vec![
+                    "between a and b".to_owned(),
+                    "between a and b".to_owned(),
+                    "between a and b".to_owned(),
+                ]),
+                Span::Module(Box::new(Module {
+                    path: PathBuf::from("./b"),
+                    spans: vec![Span::Lines(vec!["begin b".to_owned(), "end b".to_owned()])],
+                })),
+                Span::Lines(vec![
+                    "between b and c".to_owned(),
+                    "between b and c".to_owned(),
+                    "between b and c".to_owned(),
+                ]),
+                Span::Module(Box::new(Module {
+                    path: PathBuf::from("./c"),
+                    spans: vec![Span::Lines(vec!["begin c".to_owned(), "end c".to_owned()])],
+                })),
+                Span::Lines(vec!["end .".to_owned()]),
+            ],
+        };
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn test_many_moules_in_deep_module() {
+        manual_resolver! {
+            struct ManualResolver {
+                "." => "mod a;\n",
+                "./a" => concat!(
+                    "mod b {\n",
+                    "    mod c;\n",
+                    "}\n",
+                ),
+                "./a/b/c" => "mod d;\n",
+                "./a/b/c/d" => concat!(
+                    "before e\n",
+                    "before e\n",
+                    "before e\n",
+                    "mod e {\n",
+                    "    mod f;\n",
+                    "}\n",
+                    "between e and g\n",
+                    "between e and g\n",
+                    "between e and g\n",
+                    "mod g;\n",
+                    "after g\n",
+                    "after g\n",
+                    "after g\n",
+                ),
+                "./a/b/c/d/e/f" => "in f",
+                "./a/b/c/d/g" => "in g",
+            }
+        }
+        let result = bundle_crate(ManualResolver {}, ConfigToml::new(""));
+        let expected = Module {
+            path: PathBuf::from("."),
+            spans: vec![Span::Module(Box::new(Module {
+                path: PathBuf::from("./a"),
+                spans: vec![Span::Module(Box::new(Module {
+                    path: PathBuf::from("./a/b"),
+                    spans: vec![Span::Module(Box::new(Module {
+                        path: PathBuf::from("./a/b/c"),
+                        spans: vec![Span::Module(Box::new(Module {
+                            path: PathBuf::from("./a/b/c/d"),
+                            spans: vec![
+                                Span::Lines(vec![
+                                    "before e".to_owned(),
+                                    "before e".to_owned(),
+                                    "before e".to_owned(),
+                                ]),
+                                Span::Module(Box::new(Module {
+                                    path: PathBuf::from("./a/b/c/d/e"),
+                                    spans: vec![Span::Module(Box::new(Module {
+                                        path: PathBuf::from("./a/b/c/d/e/f"),
+                                        spans: vec![Span::Lines(vec!["in f".to_owned()])],
+                                    }))],
+                                })),
+                                Span::Lines(vec![
+                                    "between e and g".to_owned(),
+                                    "between e and g".to_owned(),
+                                    "between e and g".to_owned(),
+                                ]),
+                                Span::Module(Box::new(Module {
+                                    path: PathBuf::from("./a/b/c/d/g"),
+                                    spans: vec![Span::Lines(vec!["in g".to_owned()])],
+                                })),
+                                Span::Lines(vec![
+                                    "after g".to_owned(),
+                                    "after g".to_owned(),
+                                    "after g".to_owned(),
+                                ]),
+                            ],
+                        }))],
+                    }))],
+                }))],
+            }))],
         };
         assert_eq!(result, expected);
     }
--- src/bundle_crate/parse_line.rs
+++ src/bundle_crate/parse_line.rs
@@ -18,9 +18,38 @@ pub fn parse_module_decl(line: &str) -> Option<String> {
     RE.captures(line).map(|captures| captures[3].to_owned())
 }
 
+pub fn parse_module_block_begin(line: &str) -> Option<String> {
+    lazy_static! {
+        static ref RE: Regex = Regex::new(concat!(
+            "^",
+            r#"\s*"#,                               // spaces
+            r#"((pub|pub\s*\([^\)]+\))\s+)?"#,      // vis (capture 1, 2)
+            r#"mod"#,                               // mod
+            r#"\s+"#,                               // space
+            r#"([a-z|A-Z|_]([a-z|A-Z|_|0-9]*))"#,   // name (capture 3, 4)
+            r#"\s*"#,                               // spaces
+            r#"\{"#,                                // opening brace
+            r#"\s*$"#,                              // spaces
+        ))
+        .unwrap();
+    }
+    RE.captures(line).map(|captures| captures[3].to_owned())
+}
+
+// Leading spaces の個数を返します。
+pub fn parse_block_end(line: &str) -> Option<usize> {
+    lazy_static! {
+        static ref RE: Regex = Regex::new(r#"^(\s*)\}\s*$"#).unwrap();
+    }
+    RE.captures(line).map(|captures| captures[1].len())
+}
+
 #[cfg(test)]
 mod tests {
-    use {super::parse_module_decl, test_case::test_case};
+    use {
+        super::{parse_block_end, parse_module_block_begin, parse_module_decl},
+        test_case::test_case,
+    };
 
     #[test_case("mod a;" => Some("a".to_owned()); "simple mod decl")]
     #[test_case("pub mod a;" => Some("a".to_owned()); "pub mod decl")]
@@ -32,8 +61,22 @@ mod tests {
     #[test_case("mod a1;" => Some("a1".to_owned()); "contains digit")]
     #[test_case("mod a_;" => Some("a_".to_owned()); "contains under")]
     #[test_case("mod _a;" => Some("_a".to_owned()); "starts with under")]
-    #[test_case("pub (path::hey)  mod  __my_42_fn  ; " => Some("__my_42_fn".to_owned()); "complicated")]
+    #[test_case("pub ( path  ::hey )  mod  __my_42_fn  ; " => Some("__my_42_fn".to_owned()); "complicated")]
     fn test_parse_module_decl(line: &str) -> Option<String> {
         parse_module_decl(line)
+    }
+
+    #[test_case("mod a {" => Some("a".to_owned()); "simple mod decl")]
+    #[test_case("    pub ( foo   ) mod  bar  { " => Some("bar".to_owned()); "complicated")]
+    fn test_parse_module_block_begin(line: &str) -> Option<String> {
+        parse_module_block_begin(line)
+    }
+
+    #[test_case("}" => Some(0); "simple block end")]
+    #[test_case("}      " => Some(0); "block end with trailing spaces")]
+    #[test_case("      }" => Some(6); "block end with leading spaces")]
+    #[test_case("   }   " => Some(3); "block end with leading and trailing spaces")]
+    fn test_parse_block_end(line: &str) -> Option<usize> {
+        parse_block_end(line)
     }
 }
