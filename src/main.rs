13bff748fd9d7762fe1a7e120a8cb3b4d9704cb8
Kana <ngtkana@gmail.com>
2021-05-28 00:08:35 +0900

    Fix resolvers to use module path.

--- src/main.rs
+++ src/main.rs
@@ -3,23 +3,23 @@ mod resolver;
 
 pub use {
     config_toml::ConfigToml,
-    resolver::{FsResolver, Resolve},
+    resolver::{CrateResolver, Resolve},
 };
 
 #[allow(unused_macros)]
 #[macro_export]
 macro_rules! manual_resolver {
-    (enum $resolver_name:ident {
-        $($path:expr => $content:expr),* $(,)?
+    (struct $resolver_name:ident {
+        $($module_path:expr => $content:expr),* $(,)?
     }) => {
-        enum $resolver_name {}
+        struct $resolver_name {}
         impl $crate::resolver::Resolve for $resolver_name {
             type B = ::std::io::BufReader<&'static [u8]>;
-            fn resolve(path: &str) -> Self::B {
+            fn resolve(&self, module_path: &::std::path::Path) -> Self::B {
                 ::std::io::BufReader::new(
-                    match path {
+                    match module_path.to_str().unwrap() {
                         $(
-                            $path => $content,
+                            $module_path => $content,
                         )*
                         _ => unreachable!(),
                     }
--- src/resolver.rs
+++ src/resolver.rs
@@ -1,21 +1,39 @@
 use std::{
     fs::File,
     io::{BufRead, BufReader},
+    path::{Path, PathBuf},
 };
 
 pub trait Resolve {
     type B: BufRead;
-    fn resolve(path: &str) -> Self::B;
+    fn resolve(&self, module_path: &Path) -> Self::B;
 }
 
-pub enum FsResolver {}
-impl Resolve for FsResolver {
+pub struct CrateResolver {
+    root: PathBuf,
+}
+impl CrateResolver {
+    pub fn new(path_to_crate_root: PathBuf) -> Self {
+        Self {
+            root: path_to_crate_root,
+        }
+    }
+}
+impl Resolve for CrateResolver {
     type B = BufReader<File>;
-    fn resolve(path: &str) -> Self::B {
-        BufReader::new(File::open(path).unwrap_or_else(|e| {
+    // NOTE: mod.rs も探したい場合はここの実装も変えましょう！
+    fn resolve(&self, module_path: &Path) -> Self::B {
+        let mut buf = self.root.clone();
+        buf.push("src");
+        buf.push(module_path);
+        buf.set_extension("rs");
+        BufReader::new(File::open(buf).unwrap_or_else(|e| {
             panic!(
-                "パスからファイルへの解決に失敗しました。path = {:?}, e = {:?}",
-                path, e
+                concat!(
+                    "モジュールパスからファイルへの解決に失敗しました。",
+                    "module_path = {:?}, e = {:?}",
+                ),
+                module_path, e
             )
         }))
     }
@@ -24,35 +42,59 @@ impl Resolve for FsResolver {
 #[cfg(test)]
 mod tests {
     use {
-        super::{FsResolver, Resolve},
+        super::{CrateResolver, Resolve},
         crate::manual_resolver,
-        std::io::Read,
+        std::{
+            io::Read,
+            path::{Path, PathBuf},
+        },
     };
 
     #[test]
     fn test_manual_resolver() {
         manual_resolver! {
-            enum ManualResolver {
+            struct ManualResolver {
                 "a" => "content of a",
                 "b" => "content of b",
             }
         }
+        let resolver = ManualResolver {};
 
         let mut s = String::new();
-        ManualResolver::resolve("a").read_to_string(&mut s).unwrap();
+        resolver
+            .resolve(Path::new("a"))
+            .read_to_string(&mut s)
+            .unwrap();
         assert_eq!(s.as_str(), "content of a");
         s.clear();
-        ManualResolver::resolve("b").read_to_string(&mut s).unwrap();
+        resolver
+            .resolve(Path::new("b"))
+            .read_to_string(&mut s)
+            .unwrap();
         assert_eq!(s.as_str(), "content of b");
     }
 
     #[test]
-    fn test_fs_resolver() {
+    fn test_resolve_depth_1() {
         let mut s = String::new();
 
-        FsResolver::resolve("testcase/test_fs_resolver.txt")
+        let crate_resolver = CrateResolver::new(PathBuf::from("testcase/sample_crate"));
+        crate_resolver
+            .resolve(Path::new("a"))
             .read_to_string(&mut s)
             .unwrap();
-        assert_eq!(s.as_str(), "Hi!\n");
+        assert_eq!(s.as_str(), "Hi, I am a.rs!\n");
+    }
+
+    #[test]
+    fn test_resolve_depth_2() {
+        let mut s = String::new();
+
+        let crate_resolver = CrateResolver::new(PathBuf::from("testcase/sample_crate"));
+        crate_resolver
+            .resolve(Path::new("b/b1"))
+            .read_to_string(&mut s)
+            .unwrap();
+        assert_eq!(s.as_str(), "Hi, I am b1.rs!\n");
     }
 }
